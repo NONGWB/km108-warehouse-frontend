de7035d0dc3e1956a71f05b604b957d7f51971e3
Wachira Boonplee <wachira.boon@kkpfg.com>
2026-01-31 13:11:37 +0700

    fix menu

--- config/menu.ts
+++ config/menu.ts
@@ -8,7 +8,7 @@ export const menuItems: MenuItem[] = [
   { id: 0, label: 'ค้นหาสินค้า', key: 'search' },
   { id: 1, label: 'จัดการสินค้า', key: 'products' },
   { id: 2, label: 'รายการเติมสต็อค', key: 'stock' },
-  { id: 3, label: 'ข้อมูลร้านค้า', key: 'contacts' },
+  { id: 3, label: 'ข้อมูลร้านค้า/เซลล์', key: 'contacts' },
 ];
 
 export const appConfig = {
